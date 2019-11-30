b80c210fb381414644dca52471eff618179c10e7
Vadim Slesarev <vadik.slesarev@yandex.ru>
2019-11-30 22:56:22 +0300

    Create Miscellaneous.h

---
+++ receiver/Miscellaneous.h
@@ -0,0 +1,19 @@
+#ifndef MISCELLANEOUS_H
+#define MISCELLANEOUS_H
+
+// Signal led
+#define LED_SIGNAL PC13
+
+// Buzzer PWM output
+#define PIN_BUZZER PA3
+
+// Sound signals frequencies and durations
+#define SOUND_FREC_RESET 1000
+#define SOUND_DUR_RESET 500
+#define SOUND_FREC_INIT 2000
+#define SOUND_DUR_INIT 500
+#define SOUND_FREC_ERROR 2000
+#define SOUND_DUR_ERROR 1000
+
+
+#endif // MISCELLANEOUS_H
